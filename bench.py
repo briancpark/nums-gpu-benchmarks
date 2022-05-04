54de703aa14c5b8f88fb7af82ea1764759e9a6fd
Brian Park <briancpark@berkeley.edu>
2022-05-03 21:20:57 -0400

    update framework, awaiting batch scripts

--- bench.py
+++ bench.py
@@ -17,38 +17,46 @@ import csv
 settings.device_grid_name = "packed"
 
 ### GLOBAL VARIABLES
-MEMORY = 16e9 # IN GIGABYTES
-NODES = 1# int(os.popen("nvidia-smi --query-gpu=name --format=csv,noheader | wc -l").read())
-print("Nodes:", NODES)
+MEMORY = 16e9  # IN GIGABYTES
+NODES = int(
+    os.popen("nvidia-smi --query-gpu=name --format=csv,noheader | wc -l").read()
+)
 TOTAL_MEMORY = NODES * MEMORY
-THEORETICAL_PEAK_64 = (7.8 * 10 ** 12) * NODES
-THEORETICAL_PEAK_32 = (15.7 * 10 ** 12) * NODES
-THEORETICAL_PEAK_TENSOR = (125 * 10 ** 12) * NODES
-THEORETICAL_PEAK_64_NO_FMA = 3916.8 * 10 ** 9
+THEORETICAL_PEAK_64 = (7.8 * 10**12) * NODES
+THEORETICAL_PEAK_32 = (15.7 * 10**12) * NODES
+THEORETICAL_PEAK_TENSOR = (125 * 10**12) * NODES
+THEORETICAL_PEAK_64_NO_FMA = 3916.8 * 10**9
 
-KILO = float(10 ** 3)
-MEGA = float(10 ** 6)
-GIGA = float(10 ** 9)
-TERA = float(10 ** 12)
+KILO = float(10**3)
+MEGA = float(10**6)
+GIGA = float(10**9)
+TERA = float(10**12)
 
 DTYPES = {
-    nps.float32 : "float32",
-    nps.float64 : "float64",
+    "int32": nps.int32,
+    "int64": nps.int64,
+    "float16": nps.float16,
+    "float32": nps.float32,
+    "float64": nps.float64,
 }
 
-cp.random.seed(1337)
-nps.random.seed(1337)
+SEED = 1337
+cp.random.seed(SEED)
+nps.random.seed(SEED)
 
 # FLOPS
 def matmul_flops(n):
-    return 2 * (n ** 3)
+    return 2 * (n**3)
+
 
 def elementwise_flops(n):
     return n
 
+
 def run_nums(backend_name, mode, ns, dtype=None):
     settings.backend_name = backend_name
-    
+    global SEED
+
     nums.init()
 
     avg_times = []
@@ -56,13 +64,19 @@ def run_nums(backend_name, mode, ns, dtype=None):
     ### INITIALIZE LOGGER
     t0 = nps.random.rand(1)
     if dtype:
-        t0 = t0.astype(dtype)
-    fh = open("nums_" + mode + "_" + DTYPES[t0.dtype] + "_" + str(NODES) + "gpus.csv", "w", newline="")
+        t0 = t0.astype(DTYPES[dtype])
+    fh = open(
+        "data/nums_" + mode + "_" + t0.dtype.__name__ + "_" + str(NODES) + "gpus.csv",
+        "w",
+        newline="",
+    )
     writer = csv.writer(fh)
     writer.writerow(["n", "time", "flops"])
     del t0
 
     for n in ns:
+        SEED += 1
+        nps.random.seed(SEED)
         if mode == "matmul":
             A = nps.random.rand(n, n)
             B = nps.random.rand(n, n)
@@ -72,14 +86,16 @@ def run_nums(backend_name, mode, ns, dtype=None):
 
         if dtype:
             # Ex: dtype=nps.float32 passed into run_nums()
-            A = A.astype(dtype)
-            B = B.astype(dtype)
-        if mode != 'mlp':    
-            A.touch()
-            B.touch()
-            print("Total memory to be used:", A.nbytes * 3)
-            print("Total memory estimated usage: {} %".format((A.nbytes * 3) / TOTAL_MEMORY))
-            print(A.shape, A.grid_shape, A.block_shape)
+            A = A.astype(DTYPES[dtype])
+            B = B.astype(DTYPES[dtype])
+
+        A.touch()
+        B.touch()
+        print("Total memory to be used:", A.nbytes * 3)
+        print(
+            "Total memory estimated usage: {} %".format((A.nbytes * 3) / TOTAL_MEMORY)
+        )
+        print(A.shape, A.grid_shape, A.block_shape)
 
         times = []
         flops = []
@@ -102,49 +118,46 @@ def run_nums(backend_name, mode, ns, dtype=None):
                 print(end - begin)
                 times.append(end - begin)
                 flops.append(elementwise_flops(n) / (end - begin))
-            elif mode == 'mlp':
-                begin = time.time()
-                run_test(n)
-                # C.touch()
-                end = time.time()
-                print(end - begin)
-                times.append(end - begin)
-                flops.append(elementwise_flops(n) / (end - begin))
 
-        # Throw away the first five results due to CuPy's Context initialization
+        # Throw away the first five results due to CuPy's context initialization
         # More here: https://docs.cupy.dev/en/stable/user_guide/performance.html
-        times.pop(0)
-        times.pop(0)
-        times.pop(0)
-        times.pop(0)
-        times.pop(0)
+        times = times[5:]
 
-        avg_time = mean(times)
-        avg_flops = mean(flops)
-        
+        avg_time, avg_flops = mean(times), mean(flops)
+
         writer.writerow([n, avg_time, avg_flops])
 
-        if mode != 'mlp':
-            del A, B, C
+        print("Average time:", avg_time)
+        print("Average TFLOPS:", avg_flops / TERA)
+
+        del A, B, C
+
 
 def run_cupy(mode, ns, dtype=None):
     cp_avg_times = []
+    global SEED
 
     ### INITIALIZE LOGGER
-    t0 = cp.random.rand(1, dtype=dtype)
-    fh = open("cupy_" + mode + "_" + t0.dtype.base.name + "_" + str(NODES) + "gpus.csv", "w", newline="")
+    t0 = cp.random.rand(1, dtype=DTYPES[dtype])
+    fh = open(
+        "data/cupy_" + mode + "_" + t0.dtype.base.name + "_" + str(NODES) + "gpus.csv",
+        "w",
+        newline="",
+    )
     writer = csv.writer(fh)
     writer.writerow(["n", "time", "flops"])
     del t0
 
     for n in ns:
+        SEED += 1
+        cp.random.seed(SEED)
         if mode == "matmul":
-            A = cp.random.rand(n, n, dtype=dtype)
-            B = cp.random.rand(n, n, dtype=dtype)
-        elif mode == "elementwise" or mode == "mlp":
-            A = cp.random.rand(n, dtype=dtype)
-            B = cp.random.rand(n, dtype=dtype)
-       
+            A = cp.random.rand(n, n, dtype=DTYPES[dtype])
+            B = cp.random.rand(n, n, dtype=DTYPES[dtype])
+        elif mode == "elementwise":
+            A = cp.random.rand(n, dtype=DTYPES[dtype])
+            B = cp.random.rand(n, dtype=DTYPES[dtype])
+
         A.device.synchronize()
         B.device.synchronize()
 
@@ -173,69 +186,54 @@ def run_cupy(mode, ns, dtype=None):
                 print(end - begin)
                 times.append(end - begin)
                 flops.append(elementwise_flops(n) / (end - begin))
-            elif mode == 'mlp':
-                begin = time.time()
-                run_test(n)
-                # C.device.synchronize()
-                end = time.time()
-                print(end - begin)
-                times.append(end - begin)
-                flops.append(elementwise_flops(n) / (end - begin))
 
-        # Throw away the first five results due to CuPy's Context initialization
+        # Throw away the first five results due to CuPy's context initialization
         # More here: https://docs.cupy.dev/en/stable/user_guide/performance.html
-        times.pop(0)
-        times.pop(0)
-        times.pop(0)
-        times.pop(0)
-        times.pop(0)
+        times = times[5:]
 
-
-        avg_time = mean(times)
-        avg_flops = mean(flops)
+        avg_time, avg_flops = mean(times), mean(flops)
 
         writer.writerow([n, avg_time, avg_flops])
 
-        del A, B
-        if mode != 'mlp':
-            del C
-    
-    # cp_avg_flops = [flops(n) / (time * 10 ** 9) for n,time in zip(ns, cp_avg_times)]
-    # avg_flops = [flops(n) / (time * 10 ** 9) for n, time in zip(ns_big, avg_times)]
-    # print(ns, avg_times)
-    # print(avg_flops)
-    # plt.figure(figsize=(10, 10))
-    # plt.plot(ns_big, avg_flops, label="NumS (8 V100s)")
-    # plt.plot(ns, cp_avg_flops, label="CuPy Single Node")
-    # # plt.plot(ns, [THEORETICAL_PEAK_64_NO_FMA for n in ns], label="Theoretical Peak No FMA", linestyle="--")
-    # # plt.plot(ns, [THEORETICAL_PEAK_64 for n in ns], label="Theoretical Peak", linestyle="--")
-    # plt.title(r"NumS GPU GFLOPS of Elementwise Operations on $n \times n$ Matrices")
-    # plt.xscale("log")
-    # # plt.yscale("log")
-    # plt.xlabel(r"Matrix Size $n$ ($n \times n$)")
-    # plt.ylabel("GFLOPS")
-    # plt.xticks(ns_big)
-    # plt.legend()
-    # plt.savefig("NumS_GPU_FLOPS_elementwise.png", dpi=400)
+        print("Average time:", avg_time)
+        print("Average TFLOPS:", avg_flops / TERA)
 
-    # plt.clf()
-
-    plt.plot(ns_big, avg_times, label="NumS (2 V100s)")
-    plt.plot(ns, cp_avg_times, label="CuPy Single Node")
-    plt.legend()
-    plt.savefig("test.png")
+        del A, B, C
 
 
+"""
+Usage:
+    python3 bench.py <package> <benchmark> <dtype> <backend> 
+
+Example:
+    python3 bench.py nums matmul float64 gpu-intra
+    python3 bench.py cupy elementwise float32
+"""
 if __name__ == "__main__":
-    ns = [1024, 2048, 4096, 8192, 16384] # This is waht single node can handle
-    ns_big = ns + [32768]
-    # run_nums("gpu-intra", "matmul", ns_big)
-    # run_cupy("matmul", ns)
-    # ns = [1 * 10 ** 8, 2 * 10 ** 8, 3 * 10 ** 8, 4 * 10 ** 8, 5 * 10 ** 8]
-    # ns_big = ns + [6 * 10 ** 8, 7 * 10 ** 8, 8 * 10 ** 8, 9 * 10 ** 8, 10 ** 9]
-    # run_nums("gpu-intra", "elementwise", ns_big)
-    # run_cupy("elementwise", ns)
-    # run_cupy("mlp", ns_big)
-    run_nums("gpu", "mlp", ns_big)
-     
-    
+    package = sys.argv[1]
+    benchmark = sys.argv[2]
+    dtype = sys.argv[3] 
+
+    t0 = cp.random.rand(1, dtype=DTYPES[dtype])
+    if package == "nums":
+        backend = sys.argv[4]
+
+    if benchmark == "matmul":
+        base_ns = list(range(4000, 32000 * int(8 / t0.nbytes), 2000))
+        ns_full = list(range(4000, 32000 * NODES * int(8 / t0.nbytes), 2000))
+        ns = []
+        for n in ns_full:
+            usage = (n ** 2) * 3 * t0.nbytes / TOTAL_MEMORY
+            if usage < 0.5:
+                ns.append(n)
+        print("Percentage of GPU memory to be used", (ns[-1] ** 2) * 3 * t0.nbytes / TOTAL_MEMORY)
+
+    elif benchmark == "elementwise":
+        base_ns = list(range(1 * 10 ** 8, 5 * int(8 / t0.nbytes) * 10 ** 8, 1 * 10 ** 8))
+        ns = list(range(1 * 10 ** 8, NODES * int(8 / t0.nbytes) * 5 * 10 ** 8, 1 * 10 ** 8))
+        print("Percentage of GPU memory to be used", ns[-1] * 3 * t0.nbytes / TOTAL_MEMORY)
+
+    if package == "nums":
+        run_nums(backend, benchmark, ns, dtype=dtype)
+    elif package == "cupy":
+        run_cupy(benchmark, base_ns, dtype=dtype)
